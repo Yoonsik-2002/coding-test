474ab7e0aed731ff73bc84a70392edcbf6ed0d90
Replit user <yoonsikp119105@gmail.com>
2024-02-20 09:41:09 +0000

    [코드업 100제] 선택실행구조 - 6070번 문제 해결

---
+++ CodeUp/[6063~6070]selective_execution_structure/6070.py
@@ -0,0 +1,9 @@
+weather = int(input())
+if (weather // 3 == 1):
+  print("spring")
+elif (weather // 3 == 2):
+  print("summer")
+elif (weather // 3 == 3):
+  print("fall")
+else:
+  print("winter")
