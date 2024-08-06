3cf74c457ba82661fe6bfc9568906c83caa4dece
Replit user <yoonsikp119105@gmail.com>
2024-08-06 00:25:58 +0000

    [코드업 100제] 종합문제 - 6082번 문제 해결

--- CodeUp/[6077~6091]comprehensive_questions/6082.py
+++ CodeUp/[6077~6091]comprehensive_questions/6082.py
@@ -20,7 +20,9 @@
 # 숫자에 규칙을 부여하여, 더욱 간결하게 구현한 369마스터 프로그램
 num = int(input())
 
-for i in range(1, num + 1):
+for i in range(1, num + 1):  
+# 1부터 num + 1까지 1씩 증가하며 반복, python의 for문은 range(start, stop, step) 형식으로 인자를 지정할 수 있으며, for i in range(1, num + 1)과 같이 step 인자를 따로 명시하지 않고 생략한 경우, 기본적으로 1씩 증가한다.
+
   if (i % 10 == 3):
     print('X', end=' ')
   elif (i % 10 == 6):
